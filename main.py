3e429b9da1881b43372d78f42b5f475aa1ec75e1
Johannes Häusler <mail@spotlightforbugs.eu>
2023-05-09 18:37:12 +0200

    Bugs gefixed

--- main.py
+++ main.py
@@ -186,123 +186,114 @@ def hangman(num_guesses):
 
 
 def log_in():
-    with sentry_sdk.start_transaction( name="log_in", op="log_in"):
-        # check if the key exists
-        import os
 
-        try:
-            key = reg.OpenKey(reg.HKEY_CURRENT_USER, "Software\\Hangman", 0, reg.KEY_READ)
-            value = reg.QueryValueEx(key, "email")
-            if value[0] is not None:
-                os.environ["HANGMAN_EMAIL"] = value[0]
-                sentry_sdk.set_user({"email": value[0]})
-                print("Erfolgreich eingeloggt!")
-                sentry_sdk.add_breadcrumb(category="info", message="User logged in")
-                return
-        except FileNotFoundError:
-            pass
 
-        # use tkinter window to ask for email-address
+    try:
+        key = reg.OpenKey(reg.HKEY_CURRENT_USER, "Software\\Hangman", 0, reg.KEY_READ)
+        value = reg.QueryValueEx(key, "email")
+        if value[0] is not None:
+            os.environ["HANGMAN_EMAIL"] = value[0]
+            sentry_sdk.set_user({"email": value[0]})
+            print("Erfolgreich eingeloggt!")
+            sentry_sdk.add_breadcrumb(category="info", message="User logged in")
+            return
+    except FileNotFoundError:
+        pass
 
-        ROOT = tk.Tk()
-        ROOT.withdraw()
-        # the input dialog
-        USER_INP = simpledialog.askstring(title="Hangman",
-                                          prompt="Bitte gib deine Email-Adresse ein, um deine Punkte zu speichern. Deine "
-                                                 "Email-Adresse wird nicht an Dritte weitergegeben.")
+    # use tkinter window to ask for email-address
 
-        if "@" not in USER_INP or "." not in USER_INP or USER_INP is None:
-            print("Bitte gib eine gültige Email-Adresse ein!")
-            sentry_sdk.add_breadcrumb(category="error", message="Invalid email")
-            log_in()
+    ROOT = tk.Tk()
+    ROOT.withdraw()
+    # the input dialog
+    USER_INP = simpledialog.askstring(title="Hangman",
+                                      prompt="Bitte gib deine Email-Adresse ein, um deine Punkte zu speichern. Deine "
+                                             "Email-Adresse wird nicht an Dritte weitergegeben.")
+
+    if "@" not in USER_INP or "." not in USER_INP or USER_INP is None:
+        print("Bitte gib eine gültige Email-Adresse ein!")
+        sentry_sdk.add_breadcrumb(category="error", message="Invalid email")
+        log_in()
+    else:
+        sentry_sdk.set_user({"email": USER_INP})
+        # set the registry key where info will be stored
+
+        # create a key at HKEY_CURRENT_USER\Software\Hangman
+        key = reg.CreateKey(reg.HKEY_CURRENT_USER, "Software\\Hangman")
+        # set the value of the key to the email address
+        reg.SetValueEx(key, "email", 0, reg.REG_SZ, USER_INP)
+        # close the key
+        reg.CloseKey(key)
+        # set the environment variable
+        os.environ["HANGMAN_EMAIL"] = USER_INP
+
+        # check if the key was created
+        key = reg.OpenKey(reg.HKEY_CURRENT_USER, "Software\\Hangman", 0, reg.KEY_READ)
+        value = reg.QueryValueEx(key, "email")
+        if value[0] == USER_INP:
+            print("Erfolgreich eingeloggt!")
+            sentry_sdk.add_breadcrumb(category="info", message="User logged in")
         else:
-            sentry_sdk.set_user({"email": USER_INP})
-            # set the registry key where info will be stored
-
-            # create a key at HKEY_CURRENT_USER\Software\Hangman
-            key = reg.CreateKey(reg.HKEY_CURRENT_USER, "Software\\Hangman")
-            # set the value of the key to the email address
-            reg.SetValueEx(key, "email", 0, reg.REG_SZ, USER_INP)
-            # close the key
-            reg.CloseKey(key)
-            # set the environment variable
-            os.environ["HANGMAN_EMAIL"] = USER_INP
-
-            # check if the key was created
-            key = reg.OpenKey(reg.HKEY_CURRENT_USER, "Software\\Hangman", 0, reg.KEY_READ)
-            value = reg.QueryValueEx(key, "email")
-            if value[0] == USER_INP:
-                print("Erfolgreich eingeloggt!")
-                sentry_sdk.add_breadcrumb(category="info", message="User logged in")
-            else:
-                print("Fehler beim Einloggen!")
-                sentry_sdk.add_breadcrumb(category="error", message="User not logged in")
-                log_in()
+            print("Fehler beim Einloggen!")
+            sentry_sdk.add_breadcrumb(category="error", message="User not logged in")
+            log_in()
 
 
 def erneut_spielen():
-    with sentry_sdk.start_transaction( name="erneut_spielen", op="erneut_spielen"):
-        print("Tippe 'j' in die Konsole, um erneut zu spielen.")
-        sentry_sdk.add_breadcrumb(category="info", message="Play again?")
-        if input().lower().startswith("j") or input().lower().startswith("y"):
-            sentry_sdk.add_breadcrumb(category="info", message="User wants to play again")
-            main()
-        else:
-            sentry_sdk.add_breadcrumb(category="info", message="User does not want to play again")
-            print("Auf Wiedersehen!")
-            sentry_sdk.add_breadcrumb(category="info", message="Game ended")
-            exit()
+    print("Tippe 'j' in die Konsole, um erneut zu spielen.")
+    sentry_sdk.add_breadcrumb(category="info", message="Play again?")
+    if input().lower().startswith("j") or input().lower().startswith("y"):
+        sentry_sdk.add_breadcrumb(category="info", message="User wants to play again")
+        main()
+    else:
+        sentry_sdk.add_breadcrumb(category="info", message="User does not want to play again")
+        print("Auf Wiedersehen!")
+        sentry_sdk.add_breadcrumb(category="info", message="Game ended")
+        exit()
 
 
 def main():
-    with sentry_sdk.start_transaction( name="main", op="main"):
-        # is the OS Windows? Then log in
-        if os.name == "nt":
-            sentry_sdk.add_breadcrumb(category="info", message="Windows OS")
-            sentry_sdk.set_context("os", {"type": "Windows"})
-            log_in()
-        else:
-            sentry_sdk.add_breadcrumb(category="info", message="Other OS type (not Windows)")
-            sentry_sdk.set_context("os", {"type": "Other"})
+    # is the OS Windows? Then log in
+    if os.name == "nt":
+        log_in()
 
-        wort = zufallswort()
-        sentry_sdk.add_breadcrumb(category="info", message=f"Word chosen: {wort}")
-        platzhalter_aktualisieren(wort, [], False)
+    wort = zufallswort()
+    sentry_sdk.add_breadcrumb(category="info", message=f"Word chosen: {wort}")
+    platzhalter_aktualisieren(wort, [], False)
 
-        erratene_buchstaben, fertig, versuche, maximale_versuche = [], False, 0, 11
-        while not fertig and versuche < maximale_versuche:
-            momentaner_stand = platzhalter_aktualisieren(wort, erratene_buchstaben,
-                                                         True)
-            erratene_buchstaben.append(erraten(wort, erratene_buchstaben))
-            neuer_stand = platzhalter_aktualisieren(wort, erratene_buchstaben,
-                                                    False)
-            fertig = "_" not in neuer_stand
-            if not fertig:
-                versuche += neuer_stand == momentaner_stand
-                print(hangman(maximale_versuche - versuche))
-            print(f"Du hast noch {maximale_versuche - versuche} Versuche")
-            erratene_buchstaben_sortiert = sorted(erratene_buchstaben)
-            # Alle kleinschreiben
-            erratene_buchstaben_sortiert = [
-                buchstabe.lower() for buchstabe in erratene_buchstaben_sortiert
-            ]
-            # Doppelte entfernen
-            erratene_buchstaben_sortiert = list(
-                dict.fromkeys(erratene_buchstaben_sortiert))
-            print(
-                f"Versuchte Buchstaben: {[buchstabe.lower() + '' for buchstabe in erratene_buchstaben_sortiert]}"
-            )
+    erratene_buchstaben, fertig, versuche, maximale_versuche = [], False, 0, 11
+    while not fertig and versuche < maximale_versuche:
+        momentaner_stand = platzhalter_aktualisieren(wort, erratene_buchstaben,
+                                                     True)
+        erratene_buchstaben.append(erraten(wort, erratene_buchstaben))
+        neuer_stand = platzhalter_aktualisieren(wort, erratene_buchstaben,
+                                                False)
+        fertig = "_" not in neuer_stand
+        if not fertig:
+            versuche += neuer_stand == momentaner_stand
+            print(hangman(maximale_versuche - versuche))
+        print(f"Du hast noch {maximale_versuche - versuche} Versuche")
+        erratene_buchstaben_sortiert = sorted(erratene_buchstaben)
+        # Alle kleinschreiben
+        erratene_buchstaben_sortiert = [
+            buchstabe.lower() for buchstabe in erratene_buchstaben_sortiert
+        ]
+        # Doppelte entfernen
+        erratene_buchstaben_sortiert = list(
+            dict.fromkeys(erratene_buchstaben_sortiert))
+        print(
+            f"Versuchte Buchstaben: {[buchstabe.lower() + '' for buchstabe in erratene_buchstaben_sortiert]}"
+        )
 
-        if fertig:
-            print(f"Du hast das Wort {wort.capitalize()} erraten!")
-            sentry_sdk.add_breadcrumb(category="info", message=f"Word guessed: {wort}")
-            erneut_spielen()
-        else:
-            print(
-                f"Du hast das Wort nicht erraten\nDas richtige Wort war {wort.capitalize()}"
-            )
-            sentry_sdk.add_breadcrumb(category="info", message=f"Word not guessed: {wort}")
-            erneut_spielen()
+    if fertig:
+        print(f"Du hast das Wort {wort.capitalize()} erraten!")
+        sentry_sdk.add_breadcrumb(category="info", message=f"Word guessed: {wort}")
+        erneut_spielen()
+    else:
+        print(
+            f"Du hast das Wort nicht erraten\nDas richtige Wort war {wort.capitalize()}"
+        )
+        sentry_sdk.add_breadcrumb(category="info", message=f"Word not guessed: {wort}")
+        erneut_spielen()
 
 
 if __name__ == "__main__":
